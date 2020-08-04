4459078da4ddae4815d76ab576a3ff8e38d9a8a9
Dmitry <d.sinner@mail.ru>
2020-08-04 20:23:47 +0700

    Finish home work

---
+++ src/main/java/Java2_homeworktwo/MyArrayDataException.java
@@ -0,0 +1,12 @@
+package Java2_homeworktwo;
+
+public class MyArrayDataException extends RuntimeException {
+
+    int i, j;
+
+    public MyArrayDataException(int i, int j) {
+        super("Wrong data. You have: row " + i + ", column " + j);
+        this.i = i;
+        this.j = j;
+    }
+}
---
+++ src/main/java/Java2_homeworktwo/MyArraySizeException.java
@@ -0,0 +1,13 @@
+package Java2_homeworktwo;
+
+public class MyArraySizeException extends RuntimeException {
+
+    int row, column;
+
+    public MyArraySizeException(int row, int column) {
+        super("Wrong data, row and column can be only 4. You have: row " + row + ", column " + column);
+        this.row = row;
+        this.column = column;
+
+    }
+}
